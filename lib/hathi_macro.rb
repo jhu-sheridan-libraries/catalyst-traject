edf17ff633a2898732d654e0d3bbb4962fafb583
acorn <amanda@MSEL-LIAPMAC06.local>
2020-08-26 11:31:25 -0400

    adding rights to hathi -test

--- lib/hathi_macro.rb
+++ lib/hathi_macro.rb
@@ -1,6 +1,8 @@
 module HathiMacro
   Marc21 = Traject::Macros::Marc21
   OCLC_CLEAN = /^\(OCoLC\)[^0-9A-Za-z]*([0-9A-Za-z]*)[^0-9A-Za-z]*$/
+  conn =  java.sql.DriverManager.getConnection( jdbc_url(true) )
+  conn = open_connection!
 
   def hathi_access
     lambda do |record, accumulator, _context|
@@ -70,7 +72,7 @@ module HathiMacro
 
   def lookup_hathi(local_id, type)
     begin
-      conn = open_connection!
+      #conn = open_connection!
       local_id = local_id.to_s
       sql = "select * from jhu_hathi_exception where bib# = #{local_id}"
       stmt = conn.createStatement()
@@ -80,10 +82,13 @@ module HathiMacro
       hathi_value = 'none'
       while (rs.next)
         #logger.info(rs.getString(type))
-        hathi_value = rs.getString(type)
+        if type == 'access'
+          hathi_value = '[' + rs.getString(type) + ',' + rs.getString('access') + ']'
+        else
+          hathi_value = rs.getString(type)
       end
-    ensure
-      conn.close
+    #ensure
+    #  conn.close
     end
     return hathi_value
   end
