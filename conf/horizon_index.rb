efdefff1c7f1d07f397b419fcc7d315e2ffb088b
Jon Fackrell <jon.fackrell@jhu.edu>
2021-04-09 16:36:19 -0600

    no message

--- conf/horizon_index.rb
+++ conf/horizon_index.rb
@@ -265,14 +265,14 @@ each_record do |record, context|
   if (context.output_hash["hathi_access"].include?('[deny,nobody]') || context.output_hash["hathi_access"].include?('[deny,pd_pvt]'))
     # Do nothing
   elsif (context.output_hash["format"] || []).include? "Online"
-    context.output_hash["access_format"] ||= []
-    context.output_hash["access_format"]  << "Online" if context.output_hash["access_format"].empty?
+    context.output_hash["access_facet"] ||= []
+    context.output_hash["access_facet"]  << "Online" if context.output_hash["access_facet"].empty?
   elsif((context.output_hash["hathi_url"] || []).any? && !((context.output_hash["hathi_url"] || []).include? "none"))
-    context.output_hash["access_format"] ||= []
-    context.output_hash["access_format"]  << "Online" if context.output_hash["access_format"].empty?
+    context.output_hash["access_facet"] ||= []
+    context.output_hash["access_facet"]  << "Online" if context.output_hash["access_facet"].empty?
   else
-    context.output_hash["access_format"] ||= []
-    context.output_hash["access_format"] << "At the Libraries" if context.output_hash["access_format"].empty?
+    context.output_hash["access_facet"] ||= []
+    context.output_hash["access_facet"] << "At the Libraries" if context.output_hash["access_facet"].empty?
   end
   # remove 'Online' from format facet because we have now added it to the access facet
   if (context.output_hash["format"] || []).include? "Online"
