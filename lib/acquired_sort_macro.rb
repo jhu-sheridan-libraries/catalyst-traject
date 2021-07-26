089b04c466bb82dd2480f20cbc8d67a6595a589c
Jon Fackrell <jon.fackrell@jhu.edu>
2021-07-26 13:27:24 -0600

    trim space in date

--- lib/acquired_sort_macro.rb
+++ lib/acquired_sort_macro.rb
@@ -70,7 +70,7 @@ module AcquiredSortMacro
       rs = stmt.executeQuery(sql)
       date = nil
       while (rs.next)
-        date = rs.getString('acquired_date')+'Z'
+        date = rs.getString('acquired_date').rstrip+'Z'
       end
       #ensure
       #  conn.close
